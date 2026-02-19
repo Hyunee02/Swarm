c1bbad2786fbb92d65d753b6eb38820fb3275d84
이승현 <shyun6240@gmail.com>
2026-02-19 16:40:26 +0900

    2026.02.19

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -7,8 +7,6 @@ public class Map : MonoBehaviour
 
     [SerializeField] int _tileSize;
 
-    Vector3 playerPos;
-
     private void Update()
     {
         MoveMap();
@@ -16,22 +14,24 @@ public class Map : MonoBehaviour
 
     public void MoveMap()
     {
-        playerPos = _player.transform.position;
+        Vector3 playerPos = _player.transform.position;
 
         foreach (Transform tile in _tiles)
         {
             Vector3 tilePos = tile.position;
-            Vector3 offset = playerPos - tilePos;
 
-            if (offset.x > tilePos.x / 2f)
-                tilePos.x += Vector3.right.x * _tileSize * 3f;
-            else if (offset.x < -tilePos.x / 2f)
-                tilePos.x += Vector3.left.x * _tileSize * 3f;
+            float offsetX = playerPos.x - tilePos.x;
+            float offsetY = playerPos.y - tilePos.y;
 
-            if (offset.y > tilePos.y / 2f)
-                tilePos.y += Vector3.up.y * _tileSize * 3f;
-            else if (offset.y < -tilePos.y / 2f)
-                tilePos.y += Vector3.down.y * _tileSize * 3f;
+            if (offsetX > _tileSize)
+                tilePos.x += _tileSize * 3f;
+            else if (offsetX < -_tileSize)
+                tilePos.x -= _tileSize * 3f;
+
+            if (offsetY > _tileSize)
+                tilePos.y += _tileSize * 3f;
+            else if (offsetY < -_tileSize)
+                tilePos.y -= _tileSize * 3f;
 
             tile.position = tilePos;
         }
